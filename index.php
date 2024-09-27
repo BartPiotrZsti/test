9cae477eb2c38315ba664dfd0209801a66d4b7e7
BartPiotr1i2021 <105204886+BartPiotrZsti@users.noreply.github.com>
2024-09-27 09:23:36 +0200

    added page, html,php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Moja nowa strona">
+        <title>Nowa strona</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Twoja nowa strona została utworzona!</h1>
+        <p>Rozgość się, a następnie prześlij pliki przez <a href="https://zsti.me/net2ftp" target="_blank">net2ftp</a>.</p>
+    </body>
+</html>
